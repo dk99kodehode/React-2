2e1290593631cdacff477787458fe37a5040bc05
dk99kodehode <dk99.kodehode@gmail.com>
2026-09-16 14:53:25 +0200

    added more buttons and styling to cookie clicker

--- src/components/Cookie-clicker/Farms.jsx
+++ src/components/Cookie-clicker/Farms.jsx
@@ -3,6 +3,41 @@ import styles from "./Farms.module.css";
 export default function Farms() {
   return (
     <div className={styles.farms}>
+      <div className={styles.settings}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div
+            style={{
+              flexDirection: "column",
+              display: "flex",
+              gap: "20px",
+              marginLeft: "2px",
+            }}
+          >
+            <button style={{ padding: "10px", backgroundColor: "brown" }}>
+              Options
+            </button>
+            <button style={{ padding: "10px", backgroundColor: "brown" }}>
+              Stats
+            </button>
+          </div>
+
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <p>random cat fact</p>
+          </div>
+
+          <div
+            style={{ flexDirection: "column", display: "flex", gap: "20px" }}
+          >
+            <button style={{ padding: "10px", backgroundColor: "brown" }}>
+              Info
+            </button>
+            <button style={{ padding: "10px", backgroundColor: "brown" }}>
+              Legacy
+            </button>
+          </div>
+        </div>
+      </div>
+
       <div className={styles.farmBorder}>
         <div className={styles.unit}></div>
         <div className={styles.unit}></div>
