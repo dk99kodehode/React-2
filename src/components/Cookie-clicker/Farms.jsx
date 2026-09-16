cc8f603208ec66408185355d397fdf33a3a0d661
dk99kodehode <dk99.kodehode@gmail.com>
2026-09-16 14:56:11 +0200

    rounded buttons

--- src/components/Cookie-clicker/Farms.jsx
+++ src/components/Cookie-clicker/Farms.jsx
@@ -13,25 +13,54 @@ export default function Farms() {
               marginLeft: "2px",
             }}
           >
-            <button style={{ padding: "10px", backgroundColor: "brown" }}>
+            <button
+              style={{
+                padding: "10px",
+                backgroundColor: "brown",
+                borderRadius: "20px",
+              }}
+            >
               Options
             </button>
-            <button style={{ padding: "10px", backgroundColor: "brown" }}>
+            <button
+              style={{
+                padding: "10px",
+                backgroundColor: "brown",
+                borderRadius: "20px",
+              }}
+            >
               Stats
             </button>
           </div>
 
-          <div style={{ display: "flex", alignItems: "center" }}>
+          <div
+            style={{
+              alignItems: "center",
+              display: "flex",
+            }}
+          >
             <p>random cat fact</p>
           </div>
 
           <div
             style={{ flexDirection: "column", display: "flex", gap: "20px" }}
           >
-            <button style={{ padding: "10px", backgroundColor: "brown" }}>
+            <button
+              style={{
+                padding: "10px",
+                backgroundColor: "brown",
+                borderRadius: "20px",
+              }}
+            >
               Info
             </button>
-            <button style={{ padding: "10px", backgroundColor: "brown" }}>
+            <button
+              style={{
+                padding: "10px",
+                backgroundColor: "brown",
+                borderRadius: "20px",
+              }}
+            >
               Legacy
             </button>
           </div>
